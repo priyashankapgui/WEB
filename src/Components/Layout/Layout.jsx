d758346c9f0d22d2fe0a35689497d989c3588070
PRIYASHANKA.P.G.U.I <110629606+priyashankapgui@users.noreply.github.com>
2024-02-17 00:00:40 +0530

    add searchBar

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -4,16 +4,18 @@ import './Layout.css'
 import Topheader from '../Topheader/Topheader'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
+import Searchbar from '../Searchbar/Searchbar'
 
 export default function Layout({ children }) {
   return (
     <div className='layout'>
-        <Topheader/>
-        <Navbar />
+        <Topheader className='Topheader'/>
+        <Navbar className='Navbar' />
+        <Searchbar className='Searchbar'/>
 
         {children}
 
-        <Footer/>
+        <Footer className='Footer'/>
     </div>
   )
 }
---
+++ src/Components/Searchbar/Searchbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { RiSearchLine } from "react-icons/ri";
+import './Searchbar.css'; 
+
+const Searchbar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedOption, setSelectedOption] = useState('all'); 
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchClick = () => {
+    onSearch(searchTerm, selectedOption); 
+  };
+
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+   
+
+    <div className="search-container">
+       
+
+  
+      <div className='drop'>
+        <select className="dropdown" value={selectedOption} onChange={handleDropdownChange}>
+        <option value="Category"> Category </option>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+      </select>
+      </div>
+
+
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      
+    
+      <button className="search-button" onClick={handleSearchClick}>
+        <RiSearchLine style={{ fontSize: '31px' }} />
+      </button>
+
+      <div className="horizontal-line"></div>
+
+    </div>
+   
+
+  
+  );
+};
+
+export default Searchbar;
