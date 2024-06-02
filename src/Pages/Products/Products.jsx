1d3537a3c979ba6fbfcf9d7c06b230d7ad05d949
PRIYASHANKA.P.G.U.I <110629606+priyashankapgui@users.noreply.github.com>
2024-06-02 20:02:27 +0530

    new upadate 6/2
    products

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-
+import "./Products.css"
+import InputLabel from "../../Components/InputLable/InputLable";
+import Square from "../../Components/Square/Square";
 import Layout from "../../Components/Layout/Layout";
+import Body from "../../Components/Body/Body";
 
 
 export default function Products() {
   return (
     <Layout>
+      <Body>
       <div className="products">
-        <h1>Products</h1>
+      <div className="Products-title">
+              <Square size={5} color="#62C96D" marginRight={2.5} />
+              <InputLabel
+                htmlFor="example"
+                color="black"
+                fontSize="1.4em"
+                fontWeight={500}
+                lineHeight="1.5"
+              >
+                Discounts
+              </InputLabel>
+              <a href="" className="products_ViewAll">View All..</a>
+              
+            </div>
 
         
       </div>
+      </Body>
       
     </Layout>
   );
