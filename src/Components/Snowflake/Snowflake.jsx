ff043d729690c12a4ff2b799445f94061003cbbe
PRIYASHANKA.P.G.U.I <110629606+priyashankapgui@users.noreply.github.com>
2024-06-23 19:57:51 +0530

    new

--- src/Components/Snowflake/Snowflake.jsx
+++ src/Components/Snowflake/Snowflake.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Snowflake.css';
+import { FaCar } from "react-icons/fa";
+import { FaSdCard } from "react-icons/fa";
 
 const Snowflake = (props) => {
     return (
         <p className="Snowflake" id={`item${props.id}`} style={props.style}>
-            *
+            <FaCar />
+            <FaSdCard 
+            color='red'/>
+
         </p>
     );
 };
