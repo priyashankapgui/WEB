76e4df171b5803857b7a4f0b74670cf704746d98
KithminiOKD <112233369+KithminiOKD@users.noreply.github.com>
2024-06-25 21:38:46 +0530

    Contact

--- src/Components/Snowflake/Snowflake.jsx
+++ src/Components/Snowflake/Snowflake.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import './Snowflake.css';
-import { FaCar } from "react-icons/fa";
-import { FaSdCard } from "react-icons/fa";
 import { BsSnow2 } from "react-icons/bs";
 
 const Snowflake = (props) => {
     return (
         <p className="Snowflake" id={`item${props.id}`} style={props.style}>
-            {/* <FaCar />
-            <FaSdCard 
-            color='red'/> */}
-            <BsSnow2 />
+            {/* <BsSnow2 /> */}
+            <img className='heartImg' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/640px-Heart_coraz%C3%B3n.svg.png" alt="" />
         </p>
     );
 };
